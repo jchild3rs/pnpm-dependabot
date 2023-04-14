544a8c29b5efa22da302e011bd421a27107d3eac
James Childers <jchilders@rent.com>
2023-04-13 20:23:56 -0400

    test

--- run-audit.sh
+++ run-audit.sh
@@ -14,12 +14,15 @@ COUNT=$((HIGH_COUNT + CRITICAL_COUNT))
 # if there are any vulnerabilities, create a branch and PR
 if [[ $COUNT -gt 0 ]]; then
   TIMESTAMP=$(date +%s)
-  PRETTY_DATE=$(date -d @$TIMESTAMP)
-  git checkout -b audit-fixes-$TIMESTAMP
+  PRETTY_DATE=$(date +"%Y-%m-%d %H:%M:%S")
+  BRANCH_NAME=audit-fixes-$TIMESTAMP
+  git config user.name "PNPM Dependabot"
+  git config user.email "<>"
+  git checkout -b $BRANCH_NAME
   pnpm audit --fix
   pnpm install --no-frozen-lockfile
   git add .
   git commit -m "fix: audit fixes"
-  git push origin audit-fixes-$TIMESTAMP
+  git push origin $BRANCH_NAME
   gh pr create --title "fix: pnpm audit fixes $PRETTY_DATE" --body "This PR fixes the following vulnerabilities: $(cat $AUDIT_FILE | jq -r '.advisories | to_entries[] | .key + " " + .value.module_name + " " + .value.title')"
 fi
