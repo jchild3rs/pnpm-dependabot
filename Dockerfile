483535439487860d8a655ceb92afd865de55ad01
James Childers <jchilders@rent.com>
2023-04-13 21:57:52 -0400

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16-alpine
-RUN apk add --no-cache ca-certificates bash curl jq
+RUN apk add --no-cache ca-certificates bash curl jq git
 RUN npm install -g pnpm@7
-COPY run-audit.sh /run-audit.sh
+COPY . .
 RUN chmod +x /run-audit.sh
 ENTRYPOINT ["/run-audit.sh"]
--- run-audit.sh
+++ run-audit.sh
@@ -23,9 +23,30 @@ if [[ $COUNT -gt 0 ]]; then
   pnpm install --no-frozen-lockfile
   git commit -am "fix: pnpm audit errors"
   git push origin $BRANCH_NAME
-  gh label create "Security" --color "ff0000"
-  gh pr create \
-    --title "fix: pnpm audit fixes $(date +"%m/%d/%Y")" \
-    --body "This PR fixes the following vulnerabilities:\n\n$(cat $AUDIT_FILE | jq -r '.advisories | to_entries[] | .key + " ###" + .value.module_name + " " + .value.title + "\n\n*References*\n" + .value.references')" \
-    --label "Security"
+
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $ACTIONS_RUNTIME_TOKEN"\
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY/labels \
+    -d '{"name":"Security","description":"Audit fixes","color":"ff0000"}'
+
+  PR_TITLE="fix: pnpm audit fixes $(date +"%m/%d/%Y")"
+  PR_BODY="This PR fixes the following vulnerabilities:\n\n$(cat $AUDIT_FILE | jq -r '.advisories | to_entries[] | .key + " ###" + .value.module_name + " " + .value.title + "\n\n*References*\n" + .value.references')"
+  PR_LABEL="Security"
+
+  #  gh label create "Security" --color "ff0000"
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $ACTIONS_RUNTIME_TOKEN"\
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/OWNER/REPO/pulls \
+    -d '{"title":"$PR_TITLE","body":"$PR_BODY","head":"$GITHUB_HEAD_REF","base":"$GITHUB_BASE_REF","label":"$PR_LABEL"}'
+
+#  gh pr create \
+#    --title "fix: pnpm audit fixes $(date +"%m/%d/%Y")" \
+#    --body "This PR fixes the following vulnerabilities:\n\n$(cat $AUDIT_FILE | jq -r '.advisories | to_entries[] | .key + " ###" + .value.module_name + " " + .value.title + "\n\n*References*\n" + .value.references')" \
+#    --label "Security"
 fi
